b6bc861077441962e72f4ff6d7cd3e5ee5447134
lauraton <nickk5926@gmail.com>
2026-05-27 23:08:07 -0300

    Se agregó una condicional para verificar si Enderson está desaprobado o no

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ let carrera = "Tecnicatura Superior en Desarrollo de Software Multiplataforma"
 let cursando = false;
 
 console.log(`${nombre} tiene ${edad} años y está en la ${carrera}. ¿Se encuentra cursando? ${cursando}`)
+
+
+let nota = 7;
+
+if (nota >= 6) {
+    console.log("Aprobado")
+}
+else {
+    console.log("Desaprobado")
+}
+
