c199758de0557229d616421db656708698db675d
lauraton <nickk5926@gmail.com>
2026-05-28 00:34:04 -0300

    Se agregó un 'for' con la funcionalidad de mostrar su tabla de multiplicación del 1 al 10.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+// Actividad 1 (Variables)
 let nombre = "Enderson";
 const edadEnder = 18;
 let carrera = "Tecnicatura Superior en Desarrollo de Software Multiplataforma"
 let cursando = false;
-const edad = 23;
+// Actividad 2 (Variables)
 let nota = 7;
+// Actividad 3 (Variables)
+const edad = 23;
+// Actividad 4 (Variables)
+const numero = 5;
+// Actividad 5 (Variables)
+
+// Actividad 6 (Variables)
+
+// Actividad 7 (Variables)
+
+// Actividad 8 (Variables)
+
+// Actividad 9 (Variables)
+
+
 
 console.log(`${nombre} tiene ${edad} años y está en la ${carrera}. ¿Se encuentra cursando? ${cursando}`)
 
@@ -29,4 +45,8 @@ else if ((edad >= 13) && (edad <= 17)) {
 
 else {
     console.log("Adulto")
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${numero} multiplicado ${i} es ${numero * i}`)
 }
