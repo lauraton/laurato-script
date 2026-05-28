7174bdde1bd514cda696a8e7796e37d1deb8e9ab
lauraton <nickk5926@gmail.com>
2026-05-27 22:41:33 -0300

    Se agregaron las variables y constantes de la información personal de mi sujeto de prueba

---
+++ index.js
@@ -0,0 +1,6 @@
+let nombre = "Enderson";
+const edad = 18;
+let carrera = "Tecnicatura Superior en Desarrollo de Software Multiplataforma"
+let cursando = false;
+
+console.log(`${nombre} tiene ${edad} años y está en la ${carrera}. ¿Se encuentra cursando? ${cursando}`)
